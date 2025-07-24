6d0d85eb37ab9def17fbabf434c836d5a5a33524
GrootTheDeveloper <ankhangluonvuituoi@gmail.com>
2025-07-25 00:47:59 +0700

    some bfs

---
+++ 2025/C++/THT/TAYNINH/2022/c.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+
+void HelloWorld(string s) {
+    cout << s;
+}
+
+signed main(){
+    HelloWorld("Print()");
+}
