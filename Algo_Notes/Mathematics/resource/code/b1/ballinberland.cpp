01054d69d44426d1a925dc724809976b8f7be05e
GrootTheDeveloper <ankhangluonvuituoi@gmail.com>
2025-08-30 02:49:53 +0700

    Combinatorics: Chap 1 upd

---
+++ Algo_Notes/Mathematics/resource/code/b1/ballinberland.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    int t; cin >> t;
+    while (t--) {
+        int a, b, k; cin >> a >> b >> k;
+        vector<int> boy(k + 1), girl(k + 1);
+        for (int i = 1; i <= k; i++) {
+            cin >> boy[i];
+        }
+        for (int i = 1; i <= k; i++) {
+            cin >> girl[i];
+        }
+        map<int,int> mb, mg;
+        mb[boy[1]]++, mg[girl[1]]++;
+        int ans = 0;
+        for (int i = 2; i <= k; i++) {
+            int group = i - 1;
+            group -= mb[boy[i]];
+            group -= mg[girl[i]];
+            ans += group;
+            mb[boy[i]]++;
+            mg[girl[i]]++;
+        }
+        cout << ans << endl;
+    }
+}
