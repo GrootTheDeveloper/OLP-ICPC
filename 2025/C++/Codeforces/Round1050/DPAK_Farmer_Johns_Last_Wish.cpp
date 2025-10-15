cbba841b5a1d876b34133bc99ad01d9d1cee65f4
GrootTheDeveloper <ankhangluonvuituoi@gmail.com>
2025-10-16 01:37:26 +0700

    upd

---
+++ 2025/C++/Codeforces/Round1050/DPAK_Farmer_Johns_Last_Wish.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#define int long long
+using namespace std;
+
+signed main() {
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        vector<int> a(n + 1);
+        for (int i = 1; i <= n; i++) cin >> a[i];
+    }
+}
--- Algo_Notes/OLP_Training_NTP/resource/code/104.cpp
+++ Algo_Notes/OLP_Training_NTP/resource/code/104.cpp
@@ -4,22 +4,36 @@ using namespace std;
 const int MAXN = 2*1e6;
 
 vector<int> prime(MAXN + 5, true);
-
+vector<int> primeNum;
 void sang() {
     prime[0] = prime[1] = false;
-    for (int i = 2; i <= MAXN; i++) {
+    for (int i = 2; i * i <= MAXN; i++) {
         if (prime[i]) {
-            for (int j = 2*i; j <= MAXN; j += i) {
+            for (int j = i*i; j <= MAXN; j += i) {
                 prime[j] = false;
             }
         }
+    }
+    for (int i = 2; i <= MAXN; i++) {
+        if (prime[i]) primeNum.push_back(i);
     }
 }
 
 signed main() {
     sang();
-    int n; cin >> n;
-    for (int i = 2; i <= n; i++) {
-        if (prime[i]) cout << i << " ";
+    int q; cin >> q;
+    while (q--) {
+        int n; cin >> n;
+        for (int p : primeNum) {
+            if (p * p > n) break;
+            if (n % p == 0) {
+                cout << p << " ";
+                while (n % p == 0) {
+                    n/=p;
+                }
+            }
+        }
+        if (n > 1) cout << n;
+        cout << endl;
     }
 }
